c69d9b032652babbe82349424685e2efeca41811
zxielle <@JahazielFaye>
2023-02-22 17:40:52 -0500

    Js. files structured and added promp functions

--- server.js
+++ server.js
@@ -1,2 +1,69 @@
 const express = require('express');
 const inquirer = require("inquirer");
+const mysql = require('mysql2/promise');
+const db = require("./db/connection");
+const consTable = require('console.table');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+//NExpress middleware
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+db.connect(err => {
+    if(err) throw err;
+    app.listen(PORT, () => {});
+});
+
+// Start the prompt functions
+function startPrompt() {
+    inquirer.prompt({
+            type: 'list',
+            name: 'menu',
+            message: 'What would you like to do?',
+            choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add A Department', 'Add A Role', 'Add An Employee', 'Update An Employee Role', 'Update An Employee Manager', 'Delete Department', 'Delete Role', 'Delete Employee'], 
+
+    }).then( answer => {
+        switch (answer.menu) {
+            case 'View All Departments':
+                viewAllDepartments();
+                break;
+            case 'View All Roles':
+                viewAllRoles();
+                break;
+            case 'View All Employees':
+                viewAllEmployees();
+                break;
+            case 'Add A Department':
+                addDepartment();
+                break;
+            case 'Add A Role':
+                addRole();
+                break;
+            case 'Add An Employee':
+                addEmployee();
+                break;
+            case 'Update An Employee Role':
+                updateEmployeeRole();
+                break;
+            case 'Update An Employee Manager':
+                updateEmployeeManager();
+                break;
+            case 'Delete Department':
+                deleteDepartment();
+                break;
+            case 'Delete Role':
+                deleteRole();
+                break;
+            case 'Delete Employee':
+                deleteEmployee();
+                break;
+        }
+    })
+ };
+
